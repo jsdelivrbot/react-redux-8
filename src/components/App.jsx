9a66911065befeebb4c1988020e402f398e80fc7
Seth Lieberman <seth.lieberman@powerhrg.com>
2016-08-17 16:40:24 -0400

    videos are being passed around from one component to another and playing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,33 @@ import React, {Component} from 'react';
 import SearchBar from './search_bar.jsx';
 import VideoList from './video_list.jsx'
 import YTSearch from 'youtube-api-search';
+import VideoDetail from './video_detail.jsx'
 
 
 const API_KEY = 'AIzaSyDfACpg3V7ZyycXVpVBDkp4Sp8qAE2O2ew';
 
+// first App boots up and videos is an empty array and selected video is null
+// then it goes down to video detail
+// a request then is called to go get a list of videos below with API key
+// when request is complete it passes the videos to this.state.videos
+// the first video in the list will be sent to selectedVideo
+// Video detail is then passed the SelectedVideo
 class App extends Component {
 	constructor(props){
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
 
+		// search callback
+		// set initial video
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			 });
 		});
 	}
 
@@ -21,7 +36,10 @@ class App extends Component {
 		return(
 			<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 			</div>
 		)
 	}
--- src/components/video_list_item.jsx
+++ src/components/video_list_item.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const VideoListItem =({video}) => {
+const VideoListItem =({video, onVideoSelect}) => {
 	const imageUrl = video.snippet.thumbnails.default.url;
 
+// when this <li> gets clicked pass the video to onVideoSelect which will pass throught to App
 	return (
-			<li className="list-group-item">
+			<li onClick={() => onVideoSelect(video)} className="list-group-item">
 				<div className="video-list media">
 					<div className ="media-left">
 						<img className="media-object" src={imageUrl}/>
